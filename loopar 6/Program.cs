94571565f92d58cd5094d3e2d47cce639e3f9a26
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-17 13:45:00 +0100

    Add project files.

---
+++ loopar 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace loopar_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+        }
+
+    }
+
+
+}
